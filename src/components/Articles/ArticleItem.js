d4e75c958d2169db4a1290924eb869c1a3199142
dimonceg <dimonceg@gmail.com>
2020-02-16 21:13:55 +0200

    article component as function

--- src/components/Articles/ArticleItem.js
+++ src/components/Articles/ArticleItem.js
@@ -1,30 +1,28 @@
 import React from "react";
 
-class ArticleItem extends React.Component {
+function ArticleItem(props) {
+  const { title, body, tags, id } = props.data
 
-  render() {
-    const { title, body, tags, id } = this.props.data
-    return (
-      <article>
-        <header>
-            <h3>{title}</h3>
-        </header>
-        <section>
-            <p>{body}</p>
-        </section>
-        <footer>
-            <div className="tags">
-              {tags.map(tag => {
-                return <button className="btn btn-xs btn-default mx-1 my-1" key={tag}>{tag}</button>
-              })}
-            </div>
-        </footer>
-        <div className="controls">
-            <button className="btn btn-danger btn-mini" onClick={() => this.props.remove(id)}>удалить</button>
-        </div>
-      </article>
-    )
-  }
+  return (
+    <article>
+      <header>
+          <h3>{title}</h3>
+      </header>
+      <section>
+          <p>{body}</p>
+      </section>
+      <footer>
+          <div className="tags">
+            {tags.map(tag => {
+              return <button className="btn btn-xs btn-default mx-1 my-1" key={tag}>{tag}</button>
+            })}
+          </div>
+      </footer>
+      <div className="controls">
+          <button className="btn btn-danger btn-mini" onClick={() => props.remove(id)}>удалить</button>
+      </div>
+    </article>
+  )
 }
 
 export default ArticleItem;
